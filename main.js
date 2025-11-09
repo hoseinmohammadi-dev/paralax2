022330a128b29a3abf031515df91f34ce68866d4
hosein mohammadi <hosein011dev@gmail.com>
2025-11-09 23:57:18 +0330

    sec2

--- main.js
+++ main.js
@@ -4,18 +4,19 @@ const sec2 = document.querySelector(".sec2");
 const sec2H2 = document.querySelector(".sec2 > h2");
 
 window.addEventListener("scroll", () => {
-    const sec1Height = sec1.offsetHeight;
-    const scrollY = window.scrollY;
+    const winT = window.scrollY;
+    const winB = winT + window.innerHeight;
+    const sec2St = sec2.offsetTop;
 
 
-    const fromBottom = sec1Height - scrollY;
 
-    hero.style.transform = `translate(-50%, ${scrollY * 0.3}px)`;
+    hero.style.transform = `translate(-50%, ${winT * 0.3}px)`;
 
-    if (fromBottom < scrollY) {
+    if (winB >= sec2St) {
         sec2H2.classList.add("fadeUp")
     }
      else {
         sec2H2.classList.remove("fadeUp")
     }
+
 });
