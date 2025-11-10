ae74e0352e5b15d248ef358c773c2c6eaa1ac511
hosein mohammadi <hosein011dev@gmail.com>
2025-11-11 01:42:56 +0330

    sec3

--- main.js
+++ main.js
@@ -2,22 +2,35 @@ const sec1 = document.querySelector(".hero");
 const hero = document.querySelector(".hero > img");
 const sec2 = document.querySelector(".sec2");
 const sec2H2 = document.querySelector(".sec2 > h2");
+const sec3 = document.querySelector(".sec3");
+const sec3H2 = document.querySelector(".sec3 > .text > h2");
 
 window.addEventListener("scroll", () => {
     const winT = window.scrollY;
     const winB = winT + window.innerHeight;
     const sec2St = sec2.offsetTop;
+    const sec3St = sec3.offsetTop;
 
 
 
     hero.style.transform = `translate(-50%, ${winT * 0.3}px)`;
 
     if (winB >= sec2St) {
-        sec2H2.classList.add("fadeUp")
+        sec2H2.classList.add("fadeUnder")
     }
     else {
-        sec2H2.classList.remove("fadeUp")
+        sec2H2.classList.remove("fadeUnder")
     }
+
+    
+    if (winB >= sec3St) {
+        sec3H2.classList.add("fadeUp")
+    }
+    else {
+        sec3H2.classList.remove("fadeUp")
+    }
+
+
 
 });
 
